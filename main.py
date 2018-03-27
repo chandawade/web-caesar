c2f7cd8b8480f2f791c717c17edab13e6208d6f1
Chanda Wade <chanda.wade@icloud.com>
2018-03-26 19:45:19 -0500

    created form

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['Debug'] = True 
+
+form = """
+<!doctype html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <form method='post'>
+        <label>Rotate by:
+            <input type="text" name="rot" value='0'/>
+        </label>
+
+        <input type="textarea" name="text" />
+        <input type="submit" /> 
+    </body>
+</html>
+"""
+
+@app.route("/")
+def index():
+    return form 
+
+
+app.run()
